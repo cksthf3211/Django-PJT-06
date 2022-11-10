9bbdb13c248a746dd3bd7607c9529ec14cf2c845
kyominkim1074 <kyominkim1074@gmail.com>
2022-11-10 15:02:22 +0900

    accounts models/forms fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,12 +4,12 @@ from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        models = get_user_model()
-        fields = [
+        model = get_user_model()
+        fields = (
             "username",
             "nickname",
             "profile_image",
-        ]
+        )
         labels = {
             "username": "아이디",
             "nickname": "닉네임",
@@ -18,17 +18,17 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
-    password = None  # 프로필 변경과 비밀번호 변경을 따로 두기 위함
+    password = None  ## profile_update에서 password를 없애기 위함. exclude로는 안됨.
 
     class Meta:
         model = get_user_model()
         fields = [
-            "nickname",
             "email",
+            "nickname",
             "profile_image",
         ]
         labels = {
-            "nickname": "닉네임",
             "email": "이메일",
+            "nickname": "닉네임",
             "profile_image": "프로필 이미지",
         }
