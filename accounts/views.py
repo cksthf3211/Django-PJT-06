0220d0faae7c31099ca3651299b60e18eba352f4
kyominkim1074 <kyominkim1074@gmail.com>
2022-11-10 11:54:39 +0900

    accounts/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout, authenticate
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from reviews.models import Review
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("reviews:index")
+        else:
+            print("error")
+            print(form.error_messages)
+    else:
+        form = CustomUserCreationForm
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
