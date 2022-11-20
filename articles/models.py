13d0dcc955183e600d2b9480361ecb9313ba7d08
1_1_LJS <jeongseob.korea@gmail.com>
2022-11-21 01:58:50 +0900

    rows

--- articles/models.py
+++ articles/models.py
@@ -4,10 +4,11 @@ from django.conf import settings
 # Create your models here.
 
 class Article(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to="article/", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='articles', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="article/", blank=True)
+
 
 
